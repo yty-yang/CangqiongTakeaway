73b387768df2da6cd008af7b7fb14b0c3b750217
wow <110139562+yty-yang@users.noreply.github.com>
2025-01-19 04:59:49 +0000

    enable disable employee account

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -31,8 +31,16 @@ public interface EmployeeMapper {
 
     /**
      * 分页查询
+     *
      * @param employeePageQueryDTO
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 根据主键动态修改属性
+     *
+     * @param employee
+     */
+    void update(Employee employee);
 }
